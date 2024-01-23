1c2836b9fd5cf350827e856a4aae666e1683488d
YellowPapayas <kmdoan@wpi.edu>
2024-01-23 16:53:00 -0500

    created pages for create account and forgot password

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,29 +3,29 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./components/LoginPage.tsx";
 import "./index.css";
 import HomePage from "./components/HomePage.tsx";
+import CreateAccount from "./components/CreateAccount.tsx";
+import ForgotPassword from "./components/ForgotPassword.tsx";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       errorElement: <div />,
       element: <LoginPage />,
-      // children: [
-      //   {
-      //     path: "",
-      //     element: <LoginPage />,
-      //   },
-      // ],
     },
     {
       path: "/home",
       errorElement: <div />,
       element: <HomePage />,
-      // children: [
-      //   {
-      //     path: "",
-      //     element: <LoginPage />,
-      //   },
-      // ],
+    },
+    {
+      path: "/createacc",
+      errorElement: <div />,
+      element: <CreateAccount />,
+    },
+    {
+      path: "/forgotpass",
+      errorElement: <div />,
+      element: <ForgotPassword />,
     },
   ]);
 
