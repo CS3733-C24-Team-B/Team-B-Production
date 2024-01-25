bd6d4f004bd93b41b32174519f5e094f398c9f28
Katy Stuparu <kastuparu@wpi.edu>
2024-01-25 16:36:05 -0500

    Fixed node get request

--- apps/backend/src/routes/get_nodes.ts
+++ apps/backend/src/routes/get_nodes.ts
@@ -5,14 +5,14 @@ const router: Router = express.Router();
 
 router.get("/", async function (req: Request, res: Response) {
 
-    const node_data = client.node.findMany();
-
+    const node_data = await client.node.findMany();
     if (node_data === null) {
         console.error("No node data found in database");
         res.sendStatus(204);    // no data
     }
     else {
         res.send(node_data);
+        console.info("Successfully sent " + node_data.length + " nodes to frontend");
     }
 
 });
