d50a11b4c2e335903754aaacac7a865313355030
Piotr Skoczylas <pkskoczylas@wpi.edu>
2024-01-25 16:38:47 -0500

    Added Username and Password harcode Added fix to fill out backround on all pages Moved the Logo a little to the right

--- apps/frontend/src/components/HomePage.tsx
+++ apps/frontend/src/components/HomePage.tsx
@@ -18,20 +18,21 @@ export default function HomePage() {
           <div className="navbar-butn">
             <a href="/home">Home</a>
           </div>
-          <div className="navbar-butn">
-            <a href="/settings">Settings</a>
-          </div>
           <div className="dropdown">
             <button className="dropbtn">Profile</button>
-            <div className="dropdown-content">
-              <a href="/home">View Profile</a>
-              <a href="/settings">Settings</a>
-              <a href="/">Log Out</a>
-            </div>
+              <div className="dropdown-content">
+                  <a href="/home">View Profile</a>
+                  <link rel="stylesheet"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+                  <a href="/settings">
+                      <div className="fa fa-gear fa-spin"></div>
+                      Settings</a>
+                  <a href="/">Log Out</a>
+              </div>
           </div>
         </div>
-        <div id="map-container">
-          <img src={firstfloor} alt="first floor" id="map-image" />
+          <div id="map-container">
+              <img src={firstfloor} alt="first floor" id="map-image" />
         </div>
 
         <Outlet />
--- apps/frontend/src/components/LoginPage.tsx
+++ apps/frontend/src/components/LoginPage.tsx
@@ -1,51 +1,85 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import  { useState } from 'react';
 //import ExampleRoute from "./routes/ExampleRoute.tsx";
 import "../index.css";
 
 export default function LoginPage() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
-  function homePage() {
-    navigate("/home");
-  }
+    function homePage() {
+        if (username === "admin" && password === "password") {
+            navigate("/home");
+        } else {
+            setErrorMessage("Incorrect username or password. Please try again.");
+        }
+    }
 
-  function createAcc() {
-    navigate("/createacc");
-  }
+    function createAcc() {
+        navigate("/createacc");
+    }
 
-  function forgotPass() {
-    navigate("/forgotpass");
-  }
+    function forgotPass() {
+        navigate("/forgotpass");
+    }
 
-  return (
-    <div className="App">
-      <body className="body">
-        <header className="App-header">Login</header>
-        <br />
-        <form>
-          <label htmlFor="email">Email:</label>
-          <br />
-          <input type="email" id="email" name="email" />
-          <br />
-          <label htmlFor="password">Password:</label>
-          <br />
-          <input type="password" id="password" name="password" />
-          <br />
-          <label className="Small-label" htmlFor="remember">
-            Remember me:
-          </label>
-          <input type="checkbox" id="remember" name="remember" />
-          <br />
-          <input type="submit" value="Log In" name="login" onClick={homePage} />
-          <br />
-          <input type="button" value="Forgot Password?" onClick={forgotPass} />
-          <br />
-          <input type="button" value="Create Account" onClick={createAcc} />
-        </form>
-        <Outlet />
-      </body>
-    </div>
-  );
+    return (
+        <div className="App">
+            <body className="body">
+            <header className="App-header">Login</header>
+            <br />
+            <form>
+                <label htmlFor="username">Username:</label>
+                <br />
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <br />
+                <label htmlFor="password">Password:</label>
+                <br />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <label className="Small-label" htmlFor="remember">
+                    Remember me:
+                </label>
+                <input type="checkbox" id="remember" name="remember" />
+                <br />
+                <input
+                    type="button"
+                    value="Log In"
+                    name="login"
+                    onClick={homePage}
+                />
+                <br />
+                <p style={{ color: "red" }}>{errorMessage}</p>
+                <input
+                    type="button"
+                    value="Forgot Password?"
+                    onClick={forgotPass}
+                />
+                <br />
+                <input
+                    type="button"
+                    value="Create Account"
+                    onClick={createAcc}
+                />
+            </form>
+            <Outlet />
+            </body>
+        </div>
+    );
 }
