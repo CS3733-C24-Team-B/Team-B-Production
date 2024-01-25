0646b0525cf9d2cb143c65d6c93b58be415a00ff
YellowPapayas <kmdoan@wpi.edu>
2024-01-25 17:27:10 -0500

    combined updated front-end with front-end-algo

--- apps/frontend/src/components/HomePage.tsx
+++ apps/frontend/src/components/HomePage.tsx
@@ -14,31 +14,31 @@ export default function HomePage() {
             <img src={logo} alt="Hospital Logo" />
           </div>
         </header>
-        <div className="navbar">
-          <div className="navbar-butn">
-            <a href="/home">Home</a>
+          <div className="navbar">
+              <div className="navbar-butn">
+                  <a href="/home">Home</a>
+              </div>
+              <div className="navbar-butn">
+                  <a href="/csvdata">CSV Data</a>
+              </div>
+              <div className="dropdown">
+                  <button className="dropbtn">Profile</button>
+                  <div className="dropdown-content">
+                      <a href="/home">View Profile</a>
+                      <link
+                          rel="stylesheet"
+                          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+                      />
+                      <a href="/settings">
+                          <div className="fa fa-gear fa-spin"></div>
+                          Settings
+                      </a>
+                      <a href="/">Log Out</a>
+                  </div>
+              </div>
           </div>
-            <div className="navbar-butn">
-                <a href="/csvdata">CSV Data</a>
-            </div>
-          <div className="dropdown">
-            <button className="dropbtn">Profile</button>
-            <div className="dropdown-content">
-              <a href="/home">View Profile</a>
-              <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-              />
-              <a href="/settings">
-                <div className="fa fa-gear fa-spin"></div>
-                Settings
-              </a>
-              <a href="/">Log Out</a>
-            </div>
-          </div>
-        </div>
-        <div id="map-container">
-          <img src={firstfloor} alt="first floor" id="map-image" />
+          <div id="map-container">
+              <img src={firstfloor} alt="first floor" id="map-image"/>
         </div>
 
         <Outlet />
