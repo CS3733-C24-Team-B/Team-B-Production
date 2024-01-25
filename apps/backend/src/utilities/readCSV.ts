dd961eec39af2fb1c254739685dd48bf5f1d3635
Katy Stuparu <kastuparu@wpi.edu>
2024-01-25 17:27:41 -0500

    Fixed \r bug (for real this time)

--- apps/backend/src/utilities/readCSV.ts
+++ apps/backend/src/utilities/readCSV.ts
@@ -24,8 +24,8 @@ export function readNodeCSV(filePath: string) {
     const nodeData = [];
 
     // read file
-    const fileContent: string = readFileSync(filePath, "utf-8").replaceAll('\r', '');
-    const lines: string[] = fileContent.split('\n');
+    const fileContent: string = readFileSync(filePath, "utf-8");
+    const lines: string[] = fileContent.split(/\r?\n/);
     lines.splice(0, 1);                     // remove 1st line (column headings)
     lines.splice(lines.length - 1, 1);      // remove last line (empty line)
 
