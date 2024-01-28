b8f450e37e91513b8a4293d0823f50c42f5662b6
YellowPapayas <kmdoan@wpi.edu>
2024-01-28 13:56:47 -0500

    started storing node data into a table in CSVData page

--- apps/frontend/src/components/CSVData.tsx
+++ apps/frontend/src/components/CSVData.tsx
@@ -2,13 +2,13 @@ import React, {useEffect, useState} from "react";
 //import { Outlet } from "react-router-dom";
 //import ExampleRoute from "./routes/ExampleRoute.tsx";
 import "../index.css";
-//import {MapNode, createNodeList} from "../../../backend/src/utilities/algorithm.ts";
+// import {MapNode} from "../../../backend/src/utilities/algorithm.ts";
 import axios from "axios";
 
 export default function CSVData() {
   //const nodes : MapNode[] = createNodeList();
-    const [nodeData, setNodeData] = useState("");
-    const [edgeData, setEdgeData] = useState("");
+    const [nodeData, setNodeData] = useState([]);
+    const [edgeData, setEdgeData] = useState([]);
   useEffect(() => {
       async function fetch() {
           try {
@@ -22,17 +22,32 @@ export default function CSVData() {
           const res3 = await axios.get("/api/db-get-edges");
 
           console.log(res.data);
-          setNodeData(JSON.stringify(res.data));
-          setEdgeData(JSON.stringify(res3.data));
+          setNodeData(res.data);
+          setEdgeData(res3.data);
       }
       fetch().then();
   }, []);
 
-  return (
+    const arrayNode = nodeData.map(({floor, building, longName}, i) =>
+        <tr key={i} >
+            <td>{longName}</td>
+            <td>{floor}</td>
+            <td>{building}</td>
+        </tr>
+    );
+
+    // GO TO apps/backend/src/utilities/readCSV.ts TO SEE WHAT DATA IS STORED IN nodeData AND edgeData ARRAYS
+    return (
     <div className="App">
       <header className="App-header">CSV Data</header>
       <br />
-        <p>{nodeData.toString()}</p>
+        <table>
+            <tr>
+                <th>Room Name</th>
+                <th>Floor</th>
+                <th>Building Name</th>
+            </tr>
+            {arrayNode}</table>
         <br/>
         <p>{edgeData.toString()}</p>
     </div>
