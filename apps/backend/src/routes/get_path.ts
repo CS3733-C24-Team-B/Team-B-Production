b20926ad9a180de1b9038b81926058e0f0d3cd87
Goulda <36556466+Goulda@users.noreply.github.com>
2024-01-29 17:08:37 -0500

    Added displaying the pathfinding path without an input

--- apps/backend/src/routes/get_path.ts
+++ apps/backend/src/routes/get_path.ts
@@ -3,27 +3,25 @@ import client from "../bin/database-connection.ts";
 import {pathFindBFS} from "../utilities/algorithm.ts";
 
 const router: Router = express.Router();
+router.get("/:startNode/:endNode/", async function (req: Request, res: Response) {
 
-router.get("/", async function (req: Request, res: Response) {
-    
-    async function getPath(startNode: string, endNode: string) {
-
-        const node_data = await client.node.findMany({
-            where: {
-                nodeID: {
-                    in: pathFindBFS(startNode,endNode)?.map(obj => obj)
-                }
+    const node_data = await client.node.findMany({
+        where: {
+            nodeID: {
+                in: pathFindBFS(req.params.startNode, req.params.endNode)?.map(obj => obj)
             }
-        });
+        }
+    });
+
         if (node_data === null) {
             console.error("No node data found in database");
             res.sendStatus(204);    // no data
         } else {
-            res.send(JSON.stringify(node_data));
+            res.send(JSON.stringify(node_data.map(obj=> obj.longName)));
             console.info("Successfully sent " + node_data.length + " nodes to frontend");
         }
 
 
-    }
+
 });
 export default router;
--- apps/frontend/src/components/PathPrinter.tsx
+++ apps/frontend/src/components/PathPrinter.tsx
@@ -1,13 +1,20 @@
-
-import React, {useState} from 'react';
-
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 export default function PathPrinter(){
 
-    const words: string[] = ['Apple', 'Banana', 'Orange'];
     // Join the array of words into a single string
-    const joinedWords = words.join(' -> ');
 
     const [speaking, setSpeaking] = useState(false);
+    const [words, setPath] = useState([""]);
+    const joinedWords = words.join(' -> ');
+
+    useEffect(() => {
+        async function fetch() {
+                const res2 = await axios.get("/api/db-get-path/WELEV00HL1/CSERV001L1");
+                setPath(res2.data);
+        }
+        fetch().then();
+    }, []);
 
     const speakArray = () => {
         if (!speaking && window.speechSynthesis) {
@@ -24,7 +31,12 @@ export default function PathPrinter(){
 
     return (
         <div>
-            <h2>Array Items</h2>
+            <h2>Hospital Path</h2>
+            <form action="/api/saveData" method="POST">
+                <label htmlFor="dataInput">Enter data:</label>
+                <input type="text" id="dataInput" name="dataInput" required/>
+                <button type="submit">Save Data</button>
+            </form>
             <p>{joinedWords}</p>
             <button onClick={speakArray} disabled={speaking}>
                 {speaking ? 'Speaking...' : 'Speak Array'}
