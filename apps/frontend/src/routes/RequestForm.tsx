2e128c81b6168c769af0f4ad22ba612c1f82dcd4
ccraneWPI <ccrane@wpi.edu>
2024-02-05 20:40:13 -0500

    Updated the way that the service request page looks to match material

--- apps/frontend/src/routes/RequestForm.tsx
+++ apps/frontend/src/routes/RequestForm.tsx
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import {Outlet} from "react-router-dom";
 import {useNavigate} from "react-router-dom";
 import "../css/serviceform_page.css";
-import axios from "axios"
+import axios from "axios";
 import {NewRequest} from "common/src/serviceRequestTypes.ts";
 import {employee} from "common/src/employee.ts";
 import Navbar from "../components/Navbar.tsx";
@@ -17,20 +17,20 @@ export default function LoginPage() {
     const navigate = useNavigate();
     const [name, setName] = useState("");
     const [roomNumber, setRoomNumber] = useState("");
-    const [infoText, setInfoText] = useState("")
+    const [infoText, setInfoText] = useState("");
 
-    async function submit(){
-        const requestSent:NewRequest = {
+    async function submit() {
+        const requestSent: NewRequest = {
             name: name,
             roomNumber: parseInt(roomNumber),
             infoText: infoText
-        }
-        const res = await axios.post("/api/service-request", requestSent,{
+        };
+        const res = await axios.post("/api/service-request", requestSent, {
             headers: {
-                "Content-Type":"application/json"
+                "Content-Type": "application/json"
             }
         });
-        if(res.status == 200) {
+        if (res.status == 200) {
             console.log("success");
         }
         navigate("/requestlist");
@@ -43,44 +43,52 @@ export default function LoginPage() {
     }
 
     return (
-        <div className="service-form-container">
-            <div className="container">
-                <h2>Create Service Request</h2>
+        <div className={"home-container"}>
+            <div className="nav-container">
+                <Navbar/>
+            </div>
+            <div className="service-form-container">
+                <div className="container">
+                    <h2>Create Service Request</h2>
 
 
-                {/*THis labels the Name of the emplyee to do the work*/}
-                <div className="input-field">
-                    <TextField id="standard-basic" label="Name" variant="standard"
-                    value = {name} onChange={(e) => {
-                        setName(e.target.value);}} type="text" required
-                />
-                </div>
+                    {/*THis labels the Name of the emplyee to do the work*/}
+                    <div className="input-field">
+                        <TextField id="standard-basic" label="Name" variant="standard"
+                                   value={name} onChange={(e) => {
+                            setName(e.target.value);
+                        }} type="text" required
+                        />
+                    </div>
 
-                {/*This lables the room number*/}
-                <div className="input-field">
-                    <TextField id="standard-basic" label="Room Number" variant="standard"
-                               type="number"
-                               value={roomNumber}
-                               onChange={(e) => setRoomNumber(e.target.value)}
-                               required
-                    />
-                </div>
+                    {/*This lables the room number*/}
+                    <div className="input-field">
+                        <TextField id="standard-basic" label="Room Number" variant="standard"
+                                   type="number"
+                                   value={roomNumber}
+                                   onChange={(e) => setRoomNumber(e.target.value)}
+                                   required
+                        />
+                    </div>
 
-                {/*This lables the room number*/}
-                <div className="input-field">
-                    <TextField id="standard-basic" label="Request Detials" variant="standard"
-                               type="text" value = {infoText} onChange={(e =>
-                    {setInfoText(e.target.value);})} required
-                    />
-                </div> <br/>
+                    {/*This lables the room number*/}
+                    <div className="input-field">
+                        <TextField id="standard-basic" label="Request Detials" variant="standard"
+                                   type="text" value={infoText} onChange={(e => {
+                            setInfoText(e.target.value);
+                        })} required
+                        />
+                    </div>
+                    <br/>
 
-                <div>
-                    <Button
-                        style={{
-                            backgroundColor: "#012D5A",
-                        }}
-                        variant="contained" onClick={submit}> Submit Request </Button>
-                    {/*<button onClick={submit}>Submit Request</button>*/}
+                    <div>
+                        <Button
+                            style={{
+                                backgroundColor: "#012D5A",
+                            }}
+                            variant="contained" onClick={submit}> Submit Request </Button>
+                        {/*<button onClick={submit}>Submit Request</button>*/}
+                    </div>
                 </div>
             </div>
         </div>
