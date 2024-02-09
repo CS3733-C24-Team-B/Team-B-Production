e61776395024510294d4c30ed7991f901ac7f3b6
YellowPapayas <kmdoan@wpi.edu>
2024-02-08 22:34:38 -0500

    got leaflet map to show nodes better

--- apps/frontend/src/components/LeafletMap.tsx
+++ apps/frontend/src/components/LeafletMap.tsx
@@ -1,64 +1,143 @@
-//import {MapContainer, ImageOverlay} from 'react-leaflet';
-import {MapContainer, Marker, Popup, ImageOverlay} from 'react-leaflet';
+import {MapContainer, Tooltip, ImageOverlay, CircleMarker} from 'react-leaflet';
 import "../css/leaflet.css";
-import {LatLngBounds, LatLng} from "leaflet";
-import {useState, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
-//import L from "leaflet";
+import {LatLng, LatLngBounds} from "leaflet";
+// import L from "leaflet";
+//import {CircleMarker, Tooltip} from "react-leaflet";
 
 export default function LeafletMap() {
     const [nodeData, setNodeData] = useState([]);
-    const [edgeData, setEdgeData] = useState([]);
+    //const [edgeData, setEdgeData] = useState([]);
+    const [nodeStart, setNodeStart] = useState("");
+    const [nodeEnd, setNodeEnd] = useState("");
+    const [drawLine, setDrawLine] = useState(false);
+    //const [pathData, setPathData] = useState([]);
+    // const [map] = useState(L.map("mapid", {
+    //     scrollWheelZoom: true,
+    //     maxBoundsViscosity: 1.0,
+    // }));
+    // map.setView([17, 25], 5);
+    // map.setMinZoom(5);
+    // map.setMaxBounds([[0, 0], [34, 50]]);
+    // map.addLayer(L.imageOverlay("src/images/00_thelowerlevel1.png", [[0, 0], [34, 50]]));
 
     useEffect(() => {
         async function fetch() {
             try {
                 const res2 = await axios.post("/api/db-insert");
                 console.log(res2.data);
-            }
-            catch{
+            } catch {
                 console.log("post error");
             }
             const res = await axios.get("/api/db-load-nodes");
-            const res3 = await axios.get("/api/db-load-edges");
+            //const res3 = await axios.get("/api/db-load-edges");
 
             setNodeData(res.data);
-            setEdgeData(res3.data);
+            //setEdgeData(res3.data);
         }
+
         fetch().then();
     }, []);
+    
+    // useEffect(() => {
+    //     async function fetch() {
+    //         const res2 = await axios.get(`/api/db-get-path/${nodeStart}/${nodeEnd}`);
+    //         setPathData(res2.data);
+    //     }
+    //     fetch().then();
+    // }, [nodeEnd, nodeStart]);
 
-    console.log(nodeData);
-    console.log(edgeData);
+    // useEffect(() => {
+    //     // setMap(L.map("mapid", {
+    //     //               scrollWheelZoom: true,
+    //     //               maxBoundsViscosity: 1.0,
+    //     // }));
+    //
+    //     const nameToXPos = (name : string) => {
+    //         return nodeData.find(({longName}) =>
+    //             name === longName
+    //         )!["xcoord"];
+    //     };
+    //
+    //     const nameToYPos = (name : string) => {
+    //         return nodeData.find(({longName}) =>
+    //             name === longName
+    //         )!["ycoord"];
+    //     };
+    //
+    //     const nameToFloor = (name : string) => {
+    //         return nodeData.find(({longName}) =>
+    //             name === longName
+    //         )!["floor"];
+    //     };
+    //
+    //     let startX = -1;
+    //     let startY = -1;
+    //     pathData.map((nr) => {
+    //         if(nameToFloor(nr) === "L1") {
+    //             if(startX >= 0 && startY >= 0) {
+    //                 new L.Polyline([new LatLng(startX, startY), new LatLng(nameToXPos(nr), nameToYPos(nr))]).addTo(map);
+    //             }
+    //             startX = nameToXPos(nr);
+    //             startY = nameToYPos(nr);
+    //         }
+    //     });
+    // }, [nodeData, pathData]);
 
+    console.log(nodeStart);
+    
     return (
-        <MapContainer center={[50, 0]} zoom={2}
-                      //minZoom={5}
+        // <div id="mapid">
+        //     {nodeData.map(({nodeID, longName, xcoord, ycoord, floor}) => (
+        //                 (floor === "L1" ?
+        //                 <CircleMarker center={new LatLng(34.8-(ycoord*34/3400), xcoord*50/5000)} radius={6} eventHandlers={{
+        //                     click: () => {
+        //                         console.log("TEST: " + longName);
+        //                         if(drawLine) {
+        //                             setNodeStart(nodeEnd);
+        //                             setNodeEnd(nodeID);
+        //                         } else {
+        //                             setNodeEnd(nodeID);
+        //                             setDrawLine(true);
+        //                         }
+        //                     }
+        //                 }}>
+        //                     <Tooltip>
+        //                         {longName + ": " + xcoord + ", " + ycoord}
+        //                     </Tooltip>
+        //                 </CircleMarker> : <></>)
+        //             ))}
+        // </div>
+        <MapContainer center={[17, 25]} zoom={5}
+                      minZoom={5}
                       scrollWheelZoom={true}
-                      // maxBoundsViscosity={1.0}
-                      // maxBounds={new LatLngBounds(new LatLng(0, 0), new LatLng(50, 100))}
+                      maxBoundsViscosity={1.0}
+                      maxBounds={new LatLngBounds(new LatLng(0, 0), new LatLng(34, 50))}
+                      id={"mapid"}
         >
-            {/*<TileLayer*/}
-            {/*    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'*/}
-            {/*    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"*/}
-            {/*    //url="src/images/00_thegroundfloor.png"*/}
-            {/*/>*/}
             <ImageOverlay
                 url="src/images/00_thelowerlevel1.png"
-                bounds={new LatLngBounds(new LatLng(0, 0), new LatLng(100, 300))}
+                bounds={new LatLngBounds(new LatLng(0, 0), new LatLng(34, 50))}
             />
-            <Marker position={[0, 300]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-            {nodeData.map(({longName, xcoord, ycoord, floor}) => (
+            {nodeData.map(({nodeID, longName, xcoord, ycoord, floor}) => (
                 (floor === "L1" ?
-                <Marker position={[ycoord*100/5000, xcoord*300/5000]}>
-                    <Popup>
+                <CircleMarker center={new LatLng(34.8-(ycoord*34/3400), xcoord*50/5000)} radius={6} eventHandlers={{
+                    click: () => {
+                        console.log("TEST: " + longName);
+                        if(drawLine) {
+                            setNodeStart(nodeEnd);
+                            setNodeEnd(nodeID);
+                        } else {
+                            setNodeEnd(nodeID);
+                            setDrawLine(true);
+                        }
+                    }
+                }}>
+                    <Tooltip>
                         {longName + ": " + xcoord + ", " + ycoord}
-                    </Popup>
-                </Marker> : <></>)
+                    </Tooltip>
+                </CircleMarker> : <></>)
             ))}
         </MapContainer>
     );
