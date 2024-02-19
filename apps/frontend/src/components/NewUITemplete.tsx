cf97fb8fd178ce578b20a1a74f7d8e4cd2ef6951
tacohouse <luisalzamora04@gmail.com>
2024-02-18 19:22:22 -0500

    created fixed topbar, fixed navbar, plus two columns in working area

--- apps/frontend/src/components/NewUITemplete.tsx
+++ apps/frontend/src/components/NewUITemplete.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import TempNavbar from "../components/TempNavbar.tsx";
+import Topbar from "../components/Topbar.tsx";
 
 export default function NewUITemplete() {
     return (
-        <div className={"BackBlue"}>
-            <TempNavbar/>
-            <div>
-                <p>ballss</p>
-                <p>ballss</p>
-                <p>ballss</p>
+        <div className={"Container"}> {/* expands area across entire screen */}
+            <Topbar/> {/* TopGreen css fixes this to the top */}
+            <TempNavbar/> {/* NavBlue css fixes this to the left */}
+            <div className={"BackBlue"}> {/* divides area below topbar into navbar and main space */}
+                <div className={"TwoColumns"}>
+                    <div className={"Purple"}></div>
+                    <div className={"Yellow"}></div>
+                </div>
             </div>
-
-
         </div>
     );
 
