944c18006301257c308dfc9997aeb337f9aa1efd
YellowPapayas <kmdoan@wpi.edu>
2024-02-18 21:42:13 -0500

    made navbar look similar to the figma, also made it functional

--- apps/frontend/src/components/TempNavbar.tsx
+++ apps/frontend/src/components/TempNavbar.tsx
@@ -1,4 +1,4 @@
-import {IconButton } from "@mui/material";
+import {Button} from "@mui/material";
 import React from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
@@ -9,21 +9,33 @@ import SettingsIcon from "@mui/icons-material/Settings";
 export default function TempNavbar() {
     return (
         <div className="NavBlue">
-            <IconButton>
-                <AccountCircleIcon/>  {/*profile icon*/}
-            </IconButton>
-            <IconButton>
-                <AdminPanelSettingsIcon/>  {/*admin icon*/}
-            </IconButton>
-            <IconButton>
-                <NavigationIcon/>  {/*naviagtion icon*/}
-            </IconButton>
-            <IconButton>
-                <DesignServicesIcon/>  {/*service request icon*/}
-            </IconButton>
-            <IconButton>
-                <SettingsIcon/>{/*settings icon*/}
-            </IconButton>
+            <div className="icon-spacing">
+                <Button sx={{color: 'white', width: '80%', textTransform: 'none'}} style={{justifyContent: "flex-start", marginLeft: '10%', fontSize: '25px', fontFamily: 'Calibri'}}
+                        startIcon={<AccountCircleIcon style={{fontSize: 40, minWidth: 80}}/>}
+                href={"/profile-info"}>
+                    Profile
+                </Button>
+                <Button sx={{color: 'white', width: '80%', textTransform: 'none'}} style={{justifyContent: "flex-start", marginLeft: '10%', fontSize: '25px', fontFamily: 'Calibri'}}
+                        startIcon={<AdminPanelSettingsIcon style={{fontSize: 40, minWidth: 80}}/>}
+                href={"/admin-viewer"}>
+                    Admin {/*admin icon*/}
+                </Button>
+                <Button sx={{color: 'white', width: '80%', textTransform: 'none'}} style={{justifyContent: "flex-start", marginLeft: '10%', fontSize: '25px', fontFamily: 'Calibri'}}
+                        startIcon={<NavigationIcon style={{fontSize: 40, minWidth: 80}}/>}
+                href={"/home"}>
+                    Navigation {/*naviagtion icon*/}
+                </Button>
+                <Button sx={{color: 'white', width: '80%', textTransform: 'none'}} style={{justifyContent: "flex-start", marginLeft: '10%', fontSize: '25px', fontFamily: 'Calibri'}}
+                        startIcon={<DesignServicesIcon style={{fontSize: 40, minWidth: 80}}/>}
+                href={"/requestform"}>
+                    Requests {/*service request icon*/}
+                </Button>
+                <Button sx={{color: 'white', width: '80%', textTransform: 'none'}} style={{justifyContent: "flex-start", marginLeft: '10%', fontSize: '25px', fontFamily: 'Calibri'}}
+                        startIcon={<SettingsIcon style={{fontSize: 40, minWidth: 80}}/>}
+                href={"/settings"}>
+                    Settings {/*settings icon*/}
+                </Button>
+            </div>
         </div>
     );
 
