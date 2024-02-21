5c48347b34819236d782e54e6c0be60117f06814
tacohouse <luisalzamora04@gmail.com>
2024-02-20 19:47:38 -0500

    created fixed topbar, fixed navbar, plus two columns in working area

--- apps/frontend/src/routes/SettingsPage.tsx
+++ apps/frontend/src/routes/SettingsPage.tsx
@@ -32,7 +32,7 @@ export default function SettingsPage() {
                         <div className="Settings-TestCard1">
                             <ThemeProvider theme={theme}> {/* Apply the Lato font theme */}
                                 <Typography sx={{margin: "30px", fontWeight: 550, fontSize: "1.35rem"}}>
-                                    Navigation Settings:
+                                    Navigation Settings
                                 </Typography>
                                 {/*START LOCATION*/}<Box style={{display: "flex", marginLeft: "5vw"}}>
                                     <Typography sx={{fontWeight: 500, fontSize: "0.9rem"}}>
@@ -40,81 +40,127 @@ export default function SettingsPage() {
                                     </Typography>
                                     <input className="Settings-Input-Fields"></input>
                                 </Box>
-
                                 {/*EVERYTHING BELOW*/}
-                                <Box sx={{display: "flex"}}>
-                                    {/*FIRST 3 CHECKBOXES*/}
-                                    <Box sx={{marginLeft: "7vw", marginTop: "7vh", display: "grid"}}>
-                                        <Box sx={{display: "flex"}}>
-                                            <input className="Settings-Checkbox-Fields" type="checkbox"
-                                                   id="custom-checkbox"/>
-                                            <Typography sx={{marginTop: "4vh", marginLeft: "1vw", fontWeight: 550, fontSize: "0.8rem"}}>
-                                                Animate Path
-                                            </Typography>
+                                <Box sx={{backgroundColor: "white", display:"flex"}}>
+                                    <Box sx={{display: "flex"}}> {/*flex downwards*/}
+                                        {/*FIRST 3 CHECKBOXES*/}
+                                        <Box sx={{marginLeft: "7vw", marginTop: "11vh", display: "grid"}}>
+                                            <Box sx={{display: "flex"}}>
+                                                <input className="Settings-Checkbox-Fields" type="checkbox"
+                                                       id="custom-checkbox"/>
+                                                <Typography sx={{
+                                                    marginBottom: "3.9vh",
+                                                    marginLeft: "1vw",
+                                                    fontWeight: 550,
+                                                    fontSize: "0.8rem"
+                                                }}>
+                                                    Animate Path
+                                                </Typography>
+                                            </Box>
+                                            <Box sx={{display: "flex"}}>
+                                                <input className="Settings-Checkbox-Fields" type="checkbox"
+                                                       id="custom-checkbox"/>
+                                                <Typography sx={{
+                                                    marginBottom: "3.5vh",
+                                                    marginLeft: "1vw",
+                                                    fontWeight: 550,
+                                                    fontSize: "0.8rem"
+                                                }}>
+                                                    Show Path
+                                                </Typography>
+                                            </Box>
+                                            <Box sx={{display: "flex"}}>
+                                                <input className="Settings-Checkbox-Fields" type="checkbox"
+                                                       id="custom-checkbox"/>
+                                                <Typography sx={{
+                                                    marginBottom: "3.5vh",
+                                                    marginLeft: "1vw",
+                                                    fontWeight: 550,
+                                                    fontSize: "0.8rem"
+                                                }}>
+                                                    Show Edges
+                                                </Typography>
+                                            </Box>
                                         </Box>
-                                        <Box sx={{display: "flex"}}>
-                                            <input className="Settings-Checkbox-Fields" type="checkbox"
-                                                   id="custom-checkbox"/>
-                                            <Typography sx={{marginTop: "4vh", marginLeft: "1vw", fontWeight: 550, fontSize: "0.8rem"}}>
-                                                Show Path
-                                            </Typography>
-                                        </Box>
-                                        <Box sx={{display: "flex"}}>
-                                            <input className="Settings-Checkbox-Fields" type="checkbox"
-                                                   id="custom-checkbox"/>
-                                            <Typography sx={{marginTop: "4vh", marginLeft: "1vw", fontWeight: 550, fontSize: "0.8rem"}}>
-                                                Show Edges
-                                            </Typography>
-                                        </Box>
-                                    </Box>
-                                    <div className="Settings-Vertical-Line"/>
-                                    {/*CHANGE COLOR*/}
-                                    <Box sx={{height:"90%", marginLeft: "4vw", marginTop: "7vh", display: "grid"}}>
-                                        {/*SELECTOR 1*/}
-                                        <Box sx={{display: "flex", height: "80%"}}>
-                                            <Typography sx={{marginTop: "3.5vh", marginLeft: "-0.5vw", fontWeight: 550, fontSize: "0.8rem"}}>
-                                                Change Node Color
-                                            </Typography>
+                                        <div className="Settings-Vertical-Line"/>
+                                        {/*CHANGE COLOR*/}
+                                        <Box sx={{ marginLeft: "4vw", marginTop: "10.7vh", display: "grid"}}>
+                                            {/*SELECTOR 1*/}
+                                            <Box sx={{display: "flex", height: "100%"}}>
+                                                <Typography sx={{
+                                                    marginTop: "0vh",
+                                                    marginLeft: "-0.5vw",
+                                                    fontWeight: 550,
+                                                    fontSize: "0.8rem"
+                                                }}>
+                                                    Change Node Color
+                                                </Typography>
 
-                                            <input className="Settings-Color-Selector1" type="color" value={(nodeColor === null ? "#3388ff" : nodeColor!)}
-                                                   onChange={(e) => {
-                                                       localStorage.setItem("nodeColor", e.target.value);
-                                                       setNodeColor(e.target.value);
-                                                   }}/>
-                                            <Button sx={{marginLeft: "1vw", marginTop: "4.4vh", height: "20%"}}
-                                                    onClick={() => {
-                                                        localStorage.removeItem("nodeColor");
-                                                        setNodeColor(null);
-                                                    }}>
-                                                Reset
-                                            </Button>
-                                        </Box>
-                                        {/*SELECTOR 2*/}
-                                        <Box sx={{display: "flex"}}>
-                                            <Typography sx={{marginTop: "0.3vh", marginLeft: "-0.5vw", fontWeight: 550, fontSize: "0.8rem"}}>
-                                                Change Edge Color
-                                            </Typography>
-                                            <input className="Settings-Color-Selector2" type="color"
-                                                   value={(edgeColor === null ? "#008000" : edgeColor!)}
-                                                   onChange={(e) => {
-                                                       localStorage.setItem("edgeColor", e.target.value);
-                                                       setEdgeColor(e.target.value);
-                                                   }}/>
-                                            <Button sx={{marginLeft: "1vw", marginTop: "1vh", height: "20%"}}
-                                                    onClick={() => {
-                                                        localStorage.removeItem("edgeColor");
-                                                        setEdgeColor(null);
-                                                    }}>
-                                                Reset
-                                            </Button>
+                                                <input className="Settings-Color-Selector1" type="color"
+                                                       value={(nodeColor === null ? "#3388ff" : nodeColor!)}
+                                                       onChange={(e) => {
+                                                           localStorage.setItem("nodeColor", e.target.value);
+                                                           setNodeColor(e.target.value);
+                                                       }}/>
+                                                <Button sx={{marginLeft: "1vw", marginTop: "-2vh", height: "100%"}}
+                                                        onClick={() => {
+                                                            localStorage.removeItem("nodeColor");
+                                                            setNodeColor(null);
+                                                        }}>
+                                                    Reset
+                                                </Button>
+                                            </Box>
+                                            {/*SELECTOR 2*/}
+                                            <Box sx={{display: "flex", height: "100%"}}>
+                                                <Typography sx={{
+                                                    marginTop: "0.0vh",
+                                                    marginLeft: "-0.5vw",
+                                                    fontWeight: 550,
+                                                    fontSize: "0.8rem"
+                                                }}>
+                                                    Change Edge Color
+                                                </Typography>
+                                                <input className="Settings-Color-Selector2" type="color"
+                                                       value={(edgeColor === null ? "#008000" : edgeColor!)}
+                                                       onChange={(e) => {
+                                                           localStorage.setItem("edgeColor", e.target.value);
+                                                           setEdgeColor(e.target.value);
+                                                       }}/>
+                                                <Button sx={{marginLeft: "1vw", marginTop: "-2.0vh", height: "100%"}}
+                                                        onClick={() => {
+                                                            localStorage.removeItem("edgeColor");
+                                                            setEdgeColor(null);
+                                                        }}>
+                                                    Reset
+                                                </Button>
+                                            </Box>
+                                            <Box sx={{height: "100%"}}>
+                                                <Typography sx={{
+                                                    marginTop: "0.0vh",
+                                                    marginLeft: "-0.5vw",
+                                                    fontWeight: 550,
+                                                    fontSize: "0.8rem",
+                                                    color: "white"
+                                                }}>
+                                                    s
+                                                </Typography>
+                                            </Box>
                                         </Box>
                                     </Box>
                                 </Box>
                             </ThemeProvider>
                         </div>
                         <img className="Settings-TestCard2" src={settingsImage}></img>
+
                     </div>
-                    <div className="Settings-TestCard3"></div>
+                    <div className="Settings-TestCard3">
+                        <ThemeProvider theme={theme}> {/* Apply the Lato font theme */}
+                            <Typography sx={{margin: "30px", fontWeight: 550, fontSize: "1.35rem"}}>
+                                General Settings
+                            </Typography>
+                        </ThemeProvider>
+                    </div>
+
                 </div>
             </div>
         </div>
