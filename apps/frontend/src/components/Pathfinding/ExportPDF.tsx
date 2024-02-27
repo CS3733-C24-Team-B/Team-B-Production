2d4efd72ba99878e12950805c16a8a7e9a08e82f
YellowPapayas <kmdoan@wpi.edu>
2024-02-26 21:20:23 -0500

    added react-qr-code to project (but cant link it to pdf)

--- apps/frontend/src/components/Pathfinding/ExportPDF.tsx
+++ apps/frontend/src/components/Pathfinding/ExportPDF.tsx
@@ -35,7 +35,7 @@ async function exportPDF(map: HTMLElement, textDirections: string[]): Promise<vo
     try {
         let startText = 0;
         const doc: JsPDF = new JsPDF("portrait", "in", [8.5, 11]);
-        for(let i = 0; i < map.children.length; i++) {
+        for (let i = 0; i < map.children.length; i++) {
             doc.setFont("Helvetica");
             doc.setFontSize(24);
             doc.setLineWidth(600);
@@ -44,16 +44,16 @@ async function exportPDF(map: HTMLElement, textDirections: string[]): Promise<vo
             doc.setLineWidth(25);
             const logo = new Image();
             logo.src = 'src/images/Brigham_and_Womens_Hospital_horiz_rgb.png';
-            doc.addImage(logo, "png", 7.5-2.175, 0.75, 2.175, 0.25);
+            doc.addImage(logo, "png", 7.5 - 2.175, 0.75, 2.175, 0.25);
             const mapImage: HTMLCanvasElement = await HTML2Canvas(map.children.item(i) as HTMLElement);
-            doc.addImage(mapImage, 1, 1.2, 6.5, mapImage.height*(6.5/mapImage.width));
-            let cursorY = mapImage.height*(6.5/mapImage.width) + 1.45;
-            for(let j = startText; j < textDirections.length; j++) {
-                if(textDirections[j].startsWith("Floor") && j !== startText) {
+            doc.addImage(mapImage, 1, 1.2, 6.5, mapImage.height * (6.5 / mapImage.width));
+            let cursorY = mapImage.height * (6.5 / mapImage.width) + 1.45;
+            for (let j = startText; j < textDirections.length; j++) {
+                if (textDirections[j].startsWith("Floor") && j !== startText) {
                     startText = j;
                     j = textDirections.length;
-                } else if(!textDirections[j].startsWith("Floor")) {
-                    const textDirection: string = (j-i) + ". " + textDirections[j];
+                } else if (!textDirections[j].startsWith("Floor")) {
+                    const textDirection: string = (j - i) + ". " + textDirections[j];
                     const directionLines: string[] = doc.splitTextToSize(textDirection, 6.5);  // Split the text into lines
                     directionLines.map((str) => {
                         doc.text(str, 1, cursorY);
@@ -61,7 +61,7 @@ async function exportPDF(map: HTMLElement, textDirections: string[]): Promise<vo
                     });
                 }
             }
-            if(i < map.children.length - 1) {
+            if (i < map.children.length - 1) {
                 doc.addPage();
             }
         }
@@ -74,12 +74,14 @@ async function exportPDF(map: HTMLElement, textDirections: string[]): Promise<vo
     }
 }
 
-export const ExportPDF = (data: {map: HTMLElement, textDirections: string[]}) => {
+export const ExportPDF = (data: { map: HTMLElement, textDirections: string[] }) => {
 
     return (
-        <Button size="small" variant="outlined" onClick={() => exportPDF(data.map, data.textDirections)}
-                style={{color:'#012D5A', borderColor: '#012D5A', fontSize: '1.5vh', width: '100%' }}>
-            Export Directions
-        </Button>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <Button size="small" variant="outlined" onClick={() => exportPDF(data.map, data.textDirections)}
+                    style={{color: '#012D5A', borderColor: '#012D5A', fontSize: '1.5vh', width: '100%'}}>
+                Export Directions
+            </Button>
+        </div>
     );
 };
